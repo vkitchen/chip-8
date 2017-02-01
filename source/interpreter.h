e007091ccde44aef13abcb2a643e9371eb8a70fc
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-02-01 20:23:43 +1300

    reformat code

---
+++ source/interpreter.h
@@ -0,0 +1,31 @@
+/*
+		INTERPRETER.H
+		-------------
+		Copyright (c) 2017 Timothy Kral, Vaughan Kitchen
+		Released under the MIT license (https://opensource.org/licenses/MIT)
+*/
+/*!
+	@file
+	@brief Chip-8 interpreter
+	@author Timothy Kral, Vaughan Kitchen
+	@copyright 2017 Timothy Kral, Vaughan Kitchen
+*/
+
+#ifndef INTERPRETER_H_
+#define INTERPRETER_H_
+
+unsigned short opcode; //currently processed opcode
+unsigned char memory[4096]; //wam size
+unsigned short pc; //program counter
+unsigned short pc2; //previous program counter, used to check if the program begins looping
+unsigned int OpsProcessed; //counts how many opcodes have been processed
+unsigned char V[0x10]; //All 16 registers, V0 to VF, 8 bit
+unsigned short I; //Adress register, 16 bit
+
+/*
+	INTERPRETER()
+	-------------
+*/
+void interpreter();
+
+#endif
--- source/main.c
+++ source/main.c
@@ -1,40 +1,41 @@
-// Main.c: Defines the entry point for the console application.
-//
+/*
+		MAIN.C
+		-------------
+		Copyright (c) 2017 Timothy Kral, Vaughan Kitchen
+		Released under the MIT license (https://opensource.org/licenses/MIT)
+*/
+/*!
+	@file
+	@brief Program entry point
+	@author Timothy Kral, Vaughan Kitchen
+	@copyright 2017 Timothy Kral, Vaughan Kitchen
+*/
 
 //#include "stdafx.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #ifdef WIN32
 	#include <io.h>
 #else
 	#include <sys/io.h>
 #endif
-
-unsigned short opcode; //currently processed opcode
-unsigned char memory[4096]; //wam size
-unsigned short pc; //program counter
-unsigned short pc2; //previous program counter, used to check if the program begins looping
-unsigned int OpsProcessed; //counts how many opcodes have been processed
-unsigned char V[0x10]; //All 16 registers, V0 to VF, 8 bit
-unsigned short I; //Adress register, 16 bit
-int running = 1;
-FILE *rom;
-int file_size;
-
-#include "interpreter.c"
+#include "interpreter.h"
 
 const char *usage = "\
 chip-8 [FILE]\n";
 
 int main(int argc, char **argv)
-{
-	if (argc != 2)
 	{
+	FILE *rom;
+	int file_size;
+	int running = 1;
+
+	if (argc != 2)
+		{
 		printf(usage);
 		exit(0);
-	}
+		}
 
 	//open the rom we're working with
 	rom = fopen(argv[1], "rb");
@@ -51,20 +52,22 @@ int main(int argc, char **argv)
 	fread(memory + 0x200, 1, 4096 - 0x200, rom);
 	fclose(rom);
 
-	while (running) {
+	while (running)
+		{
 		//Get and decode opcode
 		opcode = memory[pc] << 8 | memory[pc + 1];
 		printf("Executing %#04x at %04X \n", opcode, pc);
 
 		//execute opcode
 		pc2 = pc;
-		Chip8Interpreter();
+		interpreter();
 		OpsProcessed++;
 
-		if (pc == pc2) {
+		if (pc == pc2)
+			{
 			printf("PC Frozen, Number of Opcodes processed: %d\n", OpsProcessed);
 			running = 0;
+			}
 		}
-	}
 	return 0;
-}
+	}
